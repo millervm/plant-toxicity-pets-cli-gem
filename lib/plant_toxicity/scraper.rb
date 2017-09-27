f0aef1058dd8db8f8e3839a5ad9f999affb4f6a9
Veronica Miller <vmcmiller@gmail.com>
2017-09-27 04:55:12 +0000

    added selectosr for links to individual letters

--- lib/plant_toxicity/scraper.rb
+++ lib/plant_toxicity/scraper.rb
@@ -2,18 +2,21 @@ class PlantToxicity::Scraper
 
   attr_accessor :doc
 
+  URL_BASE = "https://www.aspca.org"
+
   def initialize
-    @doc = Nokogiri::HTML(open("https://www.aspca.org/pet-care/animal-poison-control/toxic-and-non-toxic-plants"))
+    @doc = Nokogiri::HTML(open(URL_BASE + "/pet-care/animal-poison-control/toxic-and-non-toxic-plants"))
   end
 
   def get_list(letter)
     # get letter base url
     # search each page of results for that letter
     # create Plant object for each result, add to array (either empty array -> each << or collect)
-    #@doc.search("div.view-content:last-of-type")
-    [1,2,3].collect do |i|
-      PlantToxicity::Plant.new("plant #{i}", "url #{i}")
-    end
+    #[1,2,3].collect do |i|
+    #  PlantToxicity::Plant.new("plant #{i}", "url #{i}")
+    #end
+    letter_details = @doc.search("div.view-content span.views-summary a").detect {|letters| letters.text == letter.upcase}
+    letter_link = URL_BASE + letter_details.attribute("href").value
   end
 
   def get_plant_details(plant)
