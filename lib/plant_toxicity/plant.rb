7439057a7f9dece8d2e2870408c0cd44b2a1e371
Veronica Miller <vmcmiller@gmail.com>
2017-09-28 15:09:11 +0000

    refactored plant show_details method

--- lib/plant_toxicity/plant.rb
+++ lib/plant_toxicity/plant.rb
@@ -14,20 +14,12 @@ class PlantToxicity::Plant
     puts "-" * header.length
     self.instance_variables.each do |var|
       if var != nil && var != "" && var != :@name && var != :@url
-        if var == :@other_names
-          puts "Other Names:  #{self.instance_variable_get(var)}"
-        elsif var == :@scientific_name
-          puts "Scientific Name:  #{self.instance_variable_get(var)}"
-        elsif var == :@toxicity
-          puts "Toxicity:  #{self.instance_variable_get(var)}"
-        elsif var == :@non_toxicity
-          puts "Non-Toxicity:  #{self.instance_variable_get(var)}"
-        elsif var == :@clinical_signs
-          puts "Clinical Signs:  #{self.instance_variable_get(var)}"
-        end
-        if var != self.instance_variables.last
-          puts "\n"
-        end
+        puts "Other Names:  #{self.instance_variable_get(var)}" if var == :@other_names
+        puts "Scientific Name:  #{self.instance_variable_get(var)}" if var == :@scientific_name
+        puts "Toxicity:  #{self.instance_variable_get(var)}" if var == :@toxicity
+        puts "Non-Toxicity:  #{self.instance_variable_get(var)}" if var == :@non_toxicity
+        puts "Clinical Signs:  #{self.instance_variable_get(var)}" if var == :@clinical_signs
+        puts "\n" if var != self.instance_variables.last
       end
     end
     puts "-" * header.length
