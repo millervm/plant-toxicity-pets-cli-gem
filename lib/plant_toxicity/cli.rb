c65b886b04ee38e8d5ae8f6f73d6331b8021272b
Veronica Miller <vmcmiller@gmail.com>
2017-09-30 12:11:59 +0000

    started refactoring, entire user interface moved to cli class

--- lib/plant_toxicity/cli.rb
+++ lib/plant_toxicity/cli.rb
@@ -1,6 +1,15 @@
 class PlantToxicity::CLI
 
+  #@@scraper = nil
   attr_reader :scraper
+
+  def initialize
+    @scraper = PlantToxicity::Scraper.new
+  end
+
+  #def self.scraper
+  #  @@scraper
+  #end
 
   def call
     puts "Welcome to the Plant Toxicity tool!"
@@ -18,8 +27,14 @@ class PlantToxicity::CLI
     until input == "exit"
       input = gets.strip.downcase
       if input.match(/\b[a-z]\b/)
-        list = PlantToxicity::List.new(input.upcase)
-        list.list_plants
+        #list = PlantToxicity::List.new(input.upcase)
+        #list.list_plants
+        list = PlantToxicity::List.all.detect {|list| list.letter == input.upcase}
+        if !list
+          list = PlantToxicity::List.new(input.upcase)
+          self.scraper.get_list(list)
+        end
+        display_list(list)
         puts "Enter a letter from A to Z to see another list of plants, or enter 'exit' to end the session."
       elsif input == "exit"
         break
@@ -28,5 +43,64 @@ class PlantToxicity::CLI
       end
     end
   end
+
+  # new
+
+  def display_list(list)
+    if !list.plants.empty?
+      puts "These are the plants that begin with the letter '#{list.letter}':"
+      list.plants.each.with_index(1) do |plant, i|
+        puts "#{i}. #{plant.name}"
+      end
+      puts "-----------------------------------"
+      puts "Would you like to see more details about a plant in this list? (Y/N)"
+      input = nil
+      until input == "n"
+        input = gets.strip.downcase
+        if input == "y"
+          show_details(list)
+          puts "Would you like more information about another plant in this list? (Y/N)"
+        elsif input == "n"
+          break
+        else
+          puts "Sorry, that is not a valid option. Please enter Y or N."
+        end
+      end
+    else
+      puts "-----------------------------------"
+      puts "There are no plants that begin with the letter '#{list.letter}'."
+      puts "-----------------------------------"
+    end
+  end
+
+  def show_details(list)
+    puts "Please enter the plant's number. (1-#{list.plants.length})"
+    input = gets.strip.to_i
+    until input >= 1 && input <= list.plants.length
+      puts "Sorry, that is not a valid option. Please enter a number."
+      input = gets.strip.to_i
+    end
+    plant = list.plants[input-1]
+    self.scraper.get_plant_details(plant)
+    display_plant(plant)
+  end
+
+  def display_plant(plant)
+    header = "Details for #{plant.name}:"
+    puts "-" * header.length
+    puts header
+    puts "-" * header.length
+    plant.details.each do |detail|
+      # change to prevent blank list fields
+      puts "Other Names:  #{plant.other_names}" if detail == :@other_names
+      puts "Scientific Name:  #{plant.scientific_name}" if detail == :@scientific_name
+      puts "Toxicity:  #{plant.toxicity}" if detail == :@toxicity
+      puts "Non-Toxicity:  #{plant.non_toxicity}" if detail == :@non_toxicity
+      puts "Clinical Signs:  #{plant.clinical_signs}" if detail == :@clinical_signs
+      puts "\n" if detail != plant.details.last
+    end
+    puts "-" * header.length
+  end
+
 
 end
--- lib/plant_toxicity/plant.rb
+++ lib/plant_toxicity/plant.rb
@@ -7,22 +7,11 @@ class PlantToxicity::Plant
     @url = url
   end
 
-  def show_details
-    header = "Details for #{@name}:"
-    puts "-" * header.length
-    puts header
-    puts "-" * header.length
-    self.instance_variables.each do |var|
-      if var != nil && var != "" && var != :@name && var != :@url
-        puts "Other Names:  #{self.instance_variable_get(var)}" if var == :@other_names
-        puts "Scientific Name:  #{self.instance_variable_get(var)}" if var == :@scientific_name
-        puts "Toxicity:  #{self.instance_variable_get(var)}" if var == :@toxicity
-        puts "Non-Toxicity:  #{self.instance_variable_get(var)}" if var == :@non_toxicity
-        puts "Clinical Signs:  #{self.instance_variable_get(var)}" if var == :@clinical_signs
-        puts "\n" if var != self.instance_variables.last
-      end
+  def details
+    # or use scraper argument from cli?
+    self.instance_variables.select do |var|
+      var != nil && var != "" && var != :@name && var != :@url
     end
-    puts "-" * header.length
   end
 
 end
